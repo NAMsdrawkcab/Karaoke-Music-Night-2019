5927c86ff009864d3a39f773b206c4822d5e0d38
NAMsdrawkcab <namhoangluu2002@gmail.com>
2023-04-11 22:55:31 -0400

    Add project files.

---
+++ Karaoke Music Night 2019/Form1.Designer.vb
@@ -0,0 +1,147 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.txtValue = New System.Windows.Forms.TextBox()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.cboOption = New System.Windows.Forms.ComboBox()
+        Me.lblTotal = New System.Windows.Forms.Label()
+        Me.lblInstructions = New System.Windows.Forms.Label()
+        Me.lblHeading = New System.Windows.Forms.Label()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Panel1.Controls.Add(Me.txtValue)
+        Me.Panel1.Controls.Add(Me.btnClear)
+        Me.Panel1.Controls.Add(Me.btnCalculate)
+        Me.Panel1.Controls.Add(Me.cboOption)
+        Me.Panel1.Controls.Add(Me.lblTotal)
+        Me.Panel1.Controls.Add(Me.lblInstructions)
+        Me.Panel1.Controls.Add(Me.lblHeading)
+        Me.Panel1.Location = New System.Drawing.Point(340, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(460, 498)
+        Me.Panel1.TabIndex = 0
+        '
+        'txtValue
+        '
+        Me.txtValue.Font = New System.Drawing.Font("Segoe UI", 10.2!, System.Drawing.FontStyle.Bold)
+        Me.txtValue.Location = New System.Drawing.Point(208, 227)
+        Me.txtValue.Name = "txtValue"
+        Me.txtValue.Size = New System.Drawing.Size(48, 30)
+        Me.txtValue.TabIndex = 6
+        '
+        'btnClear
+        '
+        Me.btnClear.Font = New System.Drawing.Font("Segoe UI", 10.2!, System.Drawing.FontStyle.Bold)
+        Me.btnClear.Location = New System.Drawing.Point(268, 319)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(144, 38)
+        Me.btnClear.TabIndex = 5
+        Me.btnClear.Text = "Clear Form"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Font = New System.Drawing.Font("Segoe UI", 10.2!, System.Drawing.FontStyle.Bold)
+        Me.btnCalculate.Location = New System.Drawing.Point(49, 319)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(144, 38)
+        Me.btnCalculate.TabIndex = 4
+        Me.btnCalculate.Text = "Total Cost"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'cboOption
+        '
+        Me.cboOption.Font = New System.Drawing.Font("Segoe UI", 10.8!)
+        Me.cboOption.FormattingEnabled = True
+        Me.cboOption.Items.AddRange(New Object() {"Number of Songs ($2.99 per song)", "Hours in Private Karaoke Booth ($8.99 per hour)"})
+        Me.cboOption.Location = New System.Drawing.Point(42, 93)
+        Me.cboOption.Name = "cboOption"
+        Me.cboOption.Size = New System.Drawing.Size(383, 33)
+        Me.cboOption.TabIndex = 3
+        Me.cboOption.Text = "Customize Your Night"
+        '
+        'lblTotal
+        '
+        Me.lblTotal.AutoSize = True
+        Me.lblTotal.Font = New System.Drawing.Font("Segoe UI", 12.0!)
+        Me.lblTotal.ForeColor = System.Drawing.SystemColors.Control
+        Me.lblTotal.Location = New System.Drawing.Point(66, 422)
+        Me.lblTotal.Name = "lblTotal"
+        Me.lblTotal.Size = New System.Drawing.Size(333, 28)
+        Me.lblTotal.TabIndex = 2
+        Me.lblTotal.Text = "Total Cost of Karaoke Night - $XX.XX"
+        '
+        'lblInstructions
+        '
+        Me.lblInstructions.AutoSize = True
+        Me.lblInstructions.Font = New System.Drawing.Font("Segoe UI", 12.0!)
+        Me.lblInstructions.ForeColor = System.Drawing.SystemColors.Control
+        Me.lblInstructions.Location = New System.Drawing.Point(129, 164)
+        Me.lblInstructions.Name = "lblInstructions"
+        Me.lblInstructions.Size = New System.Drawing.Size(216, 28)
+        Me.lblInstructions.TabIndex = 1
+        Me.lblInstructions.Text = "XXXXXXXXXXXXXXXXX"
+        '
+        'lblHeading
+        '
+        Me.lblHeading.AutoSize = True
+        Me.lblHeading.Font = New System.Drawing.Font("Segoe UI", 16.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblHeading.ForeColor = System.Drawing.SystemColors.Control
+        Me.lblHeading.Location = New System.Drawing.Point(90, 21)
+        Me.lblHeading.Name = "lblHeading"
+        Me.lblHeading.Size = New System.Drawing.Size(292, 38)
+        Me.lblHeading.TabIndex = 0
+        Me.lblHeading.Text = "Karaoke Music Night"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.ClientSize = New System.Drawing.Size(800, 498)
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "Form1"
+        Me.Text = "Karaoke Music Night"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents txtValue As TextBox
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnCalculate As Button
+    Friend WithEvents cboOption As ComboBox
+    Friend WithEvents lblTotal As Label
+    Friend WithEvents lblInstructions As Label
+    Friend WithEvents lblHeading As Label
+End Class
---
+++ Karaoke Music Night 2019/Form1.vb
@@ -0,0 +1,101 @@
+﻿Public Class Form1
+    Private _decSong As Decimal = 2.99D
+    Private _decHourlyRate As Decimal = 8.99D
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Threading.Thread.Sleep(3000)
+        ClearForm()
+    End Sub
+    Private Function ValidateInput() As Boolean
+        Dim intNumber As Integer
+        Dim blnValid As Boolean = False
+        Try
+            intNumber = Convert.ToInt32(txtValue.Text)
+            If intNumber > 0D Then
+                blnValid = True
+            Else
+                MsgBox("Please enter a number greater than 0", , "Error")
+                txtValue.Focus()
+                txtValue.Clear()
+            End If
+        Catch Exception As FormatException
+            MsgBox("Please enter a valid amount", , "Error")
+            txtValue.Focus()
+            txtValue.Clear()
+        Catch Exception As OverflowException
+            MsgBox("please enter a reasonable amount", , "Error")
+            txtValue.Focus()
+            txtValue.Clear()
+        Catch Exception As SystemException
+            MsgBox("Entry invalid. Please enter a valid number representing the number of people in your party", , "Error")
+            txtValue.Focus()
+            txtValue.Clear()
+        End Try
+        Return blnValid
+    End Function
+
+    Private Function ComputeSongCost(ByVal intPass As Integer) As Decimal
+        Dim decPassCost As Decimal
+        decPassCost = intPass * _decSong
+
+        Return decPassCost
+    End Function
+
+    Private Function ComputeRoomCost(ByVal intPass As Integer) As Decimal
+        Dim decTicketCost As Decimal
+        decTicketCost = intPass * _decHourlyRate
+
+        Return decTicketCost
+    End Function
+
+    Private Sub ClearForm()
+        cboOption.SelectedIndex = -1
+        lblInstructions.Visible = False
+        txtValue.Visible = False
+        btnCalculate.Visible = False
+        btnClear.Visible = False
+        lblTotal.Visible = False
+        txtValue.Clear()
+    End Sub
+
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        Dim blnIsValid As Boolean = False
+        Dim intValue As Integer
+        Dim decTotal As Decimal
+        blnIsValid = ValidateInput()
+        If blnIsValid = True Then
+            intValue = Convert.ToInt32(txtValue.Text)
+            If cboOption.SelectedIndex = 0 Then
+                decTotal = ComputeSongCost(intValue)
+            Else
+                decTotal = ComputeRoomCost(intValue)
+            End If
+            lblTotal.Visible = True
+            lblTotal.Text = "Total Cost of Karaoke Night - " & decTotal.ToString("C")
+        End If
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        ClearForm()
+    End Sub
+
+    Private Sub cboOption_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cboOption.SelectedIndexChanged
+        If cboOption.SelectedIndex = 0 Then
+            lblInstructions.Text = "Number of Karaoke Songs"
+            lblInstructions.Visible = True
+            lblTotal.Visible = False
+            txtValue.Visible = True
+            btnCalculate.Visible = True
+            btnClear.Visible = True
+            txtValue.Focus()
+        End If
+        If cboOption.SelectedIndex = 1 Then
+            lblInstructions.Text = "Hourly Rental of Karaoke Room"
+            lblInstructions.Visible = True
+            lblTotal.Visible = False
+            txtValue.Visible = True
+            btnCalculate.Visible = True
+            btnClear.Visible = True
+            txtValue.Focus()
+        End If
+    End Sub
+End Class
